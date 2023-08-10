fdf062edff6e40d5a2a4f02be62db125e4dd1fe1
严伟浩 <ywhmatrix@gmail.com>
2023-08-10 09:34:48 +0800

    char0.1

--- src/Client/ChatClient.java
+++ src/Client/ChatClient.java
@@ -1,7 +1,15 @@
 package Client;
 
-public class ChatClient {
-    public static void main(String[] args) {
 
+import java.awt.*;
+
+public class ChatClient extends Frame {
+    public static void main(String[] args) {
+        new ChatClient().window();
+    }
+    public void window(){
+       setLocation(300,300);
+       this.setSize(400,400);
+       setVisible(true);
     }
 }
