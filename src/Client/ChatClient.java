1be8e1fa4f07840e3f4d16e811cd2c4e1b8df597
严伟浩 <ywhmatrix@gmail.com>
2023-08-10 09:58:08 +0800

    char0.2-添加输入和显示的显示框架

--- src/Client/ChatClient.java
+++ src/Client/ChatClient.java
@@ -1,15 +1,21 @@
 package Client;
 
-
 import java.awt.*;
 
 public class ChatClient extends Frame {
+    //用于响应输入事件
+    TextField tfTxt = new TextField();
+    //用于响应显示事件
+    TextArea taContent = new TextArea();
     public static void main(String[] args) {
         new ChatClient().window();
     }
     public void window(){
        setLocation(300,300);
        this.setSize(400,400);
+       add(tfTxt,BorderLayout.SOUTH);
+       add(taContent,BorderLayout.NORTH);
        setVisible(true);
+       pack();
     }
 }
