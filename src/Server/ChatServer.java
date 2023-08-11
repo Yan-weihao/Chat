ec9bfe9971fcea5fb79df1d72357e5bf354ada5b
严伟浩 <ywhmatrix@gmail.com>
2023-08-11 09:20:17 +0800

    char1.0 --Server端创建一个Cinen类，用于多个客户端的连接

--- src/Server/ChatServer.java
+++ src/Server/ChatServer.java
@@ -1,18 +1,29 @@
 package Server;
 
+import com.sun.deploy.util.SessionState;
+
+import javax.swing.*;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatServer {
-
+    List <Client> clients = new ArrayList<Client>();//存放线程
+    DataInputStream dis =null;
+    DataOutputStream dou = null;
     public static void main(String[] args) {
-        Socket s = null;
+        new ChatServer().Stata();
+
+    }
+    public void Stata (){
+
         boolean started = false;
-        DataInputStream dis = null;
         ServerSocket Ss =null;
         try {
             Ss = new ServerSocket(8888);
@@ -27,16 +38,13 @@ public class ChatServer {
         }
 
         try{
-             started = true;
+            started = true;
             while (started){
                 boolean bConnected = false;
-                s= Ss.accept();
-                bConnected =true;
-                dis = new DataInputStream(s.getInputStream());
-                while (bConnected){
-                    String str = dis.readUTF();
-                    System.out.println(str);
-                }
+                Socket s= Ss.accept();
+                Client c = new Client(s);
+                new Thread(c).start();//启动线程
+
             }
         } catch (EOFException e) {
             System.out.println("Client closed! ");
@@ -55,12 +63,38 @@ public class ChatServer {
 
     }
 
-    class Client implements Runnable{
-        Socket Ss;
+    void Send (String str){
+        try {
+            dou.writeUTF(str);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+
+    class Client implements Runnable{
+        private Socket Ss;
+        private DataInputStream dis;
+        private boolean bConnected = false;
+        Client(Socket Ss){
+            try {
+                bConnected = true;
+                dis = new DataInputStream(Ss.getInputStream());
+                bConnected = true;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
         @Override
         public void run() {
-
+            try {
+                while (bConnected){
+                    String str = dis.readUTF();
+                    System.out.println(str);
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 }
